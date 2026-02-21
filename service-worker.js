9d7d040e249e3cdcd4d2105b00641503839deb73
lawhite-bank-demo <lawihtenba@gmail.com>
2026-02-21 12:00:50 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("app-cache").then(cache =>
+      cache.addAll([
+        "index.html",
+        "dashboard.html",
+        "profile.html"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
